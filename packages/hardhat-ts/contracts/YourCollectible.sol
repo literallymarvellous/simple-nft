bee69e11f1ff17309b29b6cd36d4c59736af8d26
literallymarvellous <ahiaraikechukwu@gmail.com>
2022-07-22 08:16:06 +0100

    added comments to contract

--- packages/hardhat-ts/contracts/YourCollectible.sol
+++ packages/hardhat-ts/contracts/YourCollectible.sol
@@ -1,23 +1,27 @@
 pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
-// import "hardhat/console.sol";
+/**
+ * @title Marvels Collectibles NFT
+ * @author Ahiara Ikechukwu
+ * @notice An example of a nft contract
+ */
+
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
-//learn more: https://docs.openzeppelin.com/contracts/3.x/erc721
-
-// GET LISTED ON OPENSEA: https://testnets.opensea.io/get-listed/step-two
-
 contract YourCollectible is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  constructor() public ERC721('MarvelCollectibles', 'MCB') {}
+  constructor() ERC721('MarvelCollectibles', 'MCB') {}
 
+  /**
+   * @notice Base URI for computing {tokenURI}
+   */
   function _baseURI() internal view virtual override returns (string memory) {
     return 'https://ipfs.io/ipfs/';
   }
@@ -30,6 +34,11 @@ contract YourCollectible is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable 
     super._beforeTokenTransfer(from, to, tokenId);
   }
 
+  /**
+   * @notice Destroys `tokenId`.
+   * @param tokenId id of the token to destroy
+   * Emits a {Transfer} event.
+   */
   function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
     super._burn(tokenId);
   }
@@ -38,16 +47,27 @@ contract YourCollectible is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable 
     return super.supportsInterface(interfaceId);
   }
 
+  /**
+   * @notice returns url of the token
+   * @param tokenId id of the token
+   */
   function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
     return super.tokenURI(tokenId);
   }
 
-  function mintItem(address to, string memory tokenURI) public returns (uint256) {
+  /**
+   * @notice Mints tokenId and transfers it to `to`.
+   * @param to address to which the token is minted
+   * @param _tokenURI url of the token to mint
+   *
+   * Emits a {Transfer} event.
+   */
+  function mintItem(address to, string memory _tokenURI) public returns (uint256) {
     _tokenIds.increment();
 
     uint256 id = _tokenIds.current();
     _mint(to, id);
-    _setTokenURI(id, tokenURI);
+    _setTokenURI(id, _tokenURI);
 
     return id;
   }
